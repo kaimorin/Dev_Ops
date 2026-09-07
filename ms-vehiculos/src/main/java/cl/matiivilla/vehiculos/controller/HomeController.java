6ee9315329833375c73b40aa7b9133a826edbbca
yva <yva.munoz@duocuc.cl>
2026-09-07 19:21:34 -0300

    feature bienvenida

--- ms-vehiculos/src/main/java/cl/matiivilla/vehiculos/controller/HomeController.java
+++ ms-vehiculos/src/main/java/cl/matiivilla/vehiculos/controller/HomeController.java
@@ -14,6 +14,7 @@ public class HomeController {
     @GetMapping("/")
     @Operation(summary = "Información de la API")
     public Map<String, String> home() {
-        return Map.of("mensaje", "Api-vehiculos, Version 2.0.0");
+        return Map.of("mensaje", "Api-vehiculos, Version 2.0.0,Desarrollado por byron e yvanni");
+        
     }
 }
